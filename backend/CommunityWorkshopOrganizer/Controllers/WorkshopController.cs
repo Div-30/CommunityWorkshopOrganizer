230b8d1b3b61404fed0002f7933549799590985a
Rutaganda <valentinrutaganda04@gmail.com>
2026-04-14 00:59:39 +0200

    added a class for workshops operations endpoint

---
+++ backend/CommunityWorkshopOrganizer/Controllers/WorkshopController.cs
@@ -0,0 +1,28 @@
+﻿using CommunityWorkshopOrganizer.Data;
+using CommunityWorkshopOrganizer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommunityWorkshopOrganizer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkshopController : ControllerBase
+    {
+        private readonly ApiContext context;
+        public WorkshopController(ApiContext apiContext)
+        {
+            context = apiContext;
+        }
+
+        [HttpPut]
+        public ActionResult<IEnumerable<Workshop>> createWorkShop([FromBody] Workshop workshop)
+        {
+            context.Workshops.Add(workshop);
+            context.SaveChanges();
+            return Ok(workshop);
+
+        }
+
+    }
+}
