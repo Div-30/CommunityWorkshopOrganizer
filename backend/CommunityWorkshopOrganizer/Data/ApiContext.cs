dbd2e94b2df26f70bd9d661f6c9693fa178b8bd5
Rutaganda <valentinrutaganda04@gmail.com>
2026-04-14 00:54:07 +0200

    added a data folder with a file which allows to make the database operations

---
+++ backend/CommunityWorkshopOrganizer/Data/ApiContext.cs
@@ -0,0 +1,16 @@
+﻿using CommunityWorkshopOrganizer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommunityWorkshopOrganizer.Data
+{
+    public class ApiContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<Workshop> Workshops { get; set; }
+        public DbSet<Registration> Registrations { get; set; }
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options) { 
+        }
+
+    }
+}
