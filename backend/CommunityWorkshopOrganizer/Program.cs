bc58521278f9bc9a07e1af41f1e9db4a03a45c1f
Rutaganda <valentinrutaganda04@gmail.com>
2026-04-14 01:01:39 +0200

    added swagger documentation for API

--- backend/CommunityWorkshopOrganizer/Program.cs
+++ backend/CommunityWorkshopOrganizer/Program.cs
@@ -1,23 +1,35 @@
+using CommunityWorkshopOrganizer.Data;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-
+builder.Services.AddDbContext<ApiContext>(options =>
+    options.UseInMemoryDatabase("CommunityWorkshopOrganizerDb"));
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//  Add this line to make backend accept frontend codes
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontEnd", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// Add this — must be BEFORE UseAuthorization and MapControllers
+app.UseCors("AllowFrontEnd");
 
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
