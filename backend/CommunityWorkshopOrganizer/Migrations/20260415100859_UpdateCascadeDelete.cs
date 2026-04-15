e27873bf7ac74cf62c1d6935bf708202b0802832
Div-30 <divinpaul88@gmail.com>
2026-04-15 12:23:10 +0200

    chore: configure JSON reference handler in Program.cs to prevent circular dependency cycles

---
+++ backend/CommunityWorkshopOrganizer/Migrations/20260415100859_UpdateCascadeDelete.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CommunityWorkshopOrganizer.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCascadeDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Registrations_Users_UserId",
+                table: "Registrations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Workshops_Users_OrganizerId",
+                table: "Workshops");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Registrations_Users_UserId",
+                table: "Registrations",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Workshops_Users_OrganizerId",
+                table: "Workshops",
+                column: "OrganizerId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Registrations_Users_UserId",
+                table: "Registrations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Workshops_Users_OrganizerId",
+                table: "Workshops");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Registrations_Users_UserId",
+                table: "Registrations",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Workshops_Users_OrganizerId",
+                table: "Workshops",
+                column: "OrganizerId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
