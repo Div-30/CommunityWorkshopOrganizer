3a330ba4ae71651bea7454e964031f72089a2103
Rutaganda <valentinrutaganda04@gmail.com>
2026-04-15 10:44:18 +0200

    implemented relationships between resources entity and other entities

--- backend/CommunityWorkshopOrganizer/Models/Resource.cs
+++ backend/CommunityWorkshopOrganizer/Models/Resource.cs
@@ -1,9 +1,13 @@
-﻿namespace CommunityWorkshopOrganizer.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommunityWorkshopOrganizer.Models
 {
     public class Resource
     {
+        [Key]
         public int ResourceId { get; set; }
-        public int WorkshopId { get; set; }
+        public int? WorkshopId { get; set; }
+        public Workshop? Workshop { get; set; }
         public string Title { get; set; } = string.Empty;
         public string ResourceUrl { get; set; } = string.Empty;
         public DateTime UploadedAt { get; set; }
