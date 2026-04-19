2d7cbef1b828128f433980691f93173f1da46ac4
Div-30 <divinpaul88@gmail.com>
2026-04-19 13:50:46 +0200

    feat: added user controller

--- backend/CommunityWorkshopOrganizer/Controllers/UserController.cs
+++ backend/CommunityWorkshopOrganizer/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CommunityWorkshopOrganizer.Models;
+using CommunityWorkshopOrganizer.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommunityWorkshopOrganizer.Controllers
@@ -7,5 +8,44 @@ namespace CommunityWorkshopOrganizer.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public ActionResult<User> CreateUser([FromBody] User user)
+        {
+            var result = _userService.CreateUser(user);
+            
+            if (!result.Success)
+            {
+                return BadRequest(result.Message);
+            }
+
+            return CreatedAtAction(nameof(GetUserById), new { id = result.Data.UserId }, result.Data);
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetAllUsers()
+        {
+            var result = _userService.GetAllUsers();
+            return Ok(result.Data);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUserById(int id)
+        {
+            var result = _userService.GetUserById(id);
+
+            if (!result.Success)
+            {
+                return NotFound(result.Message);
+            }
+
+            return Ok(result.Data);
+        }
     }
 }
