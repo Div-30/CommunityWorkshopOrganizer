bd77965141492f31d4c6f7a3759a3e244dbc82b0
Div-30 <divinpaul88@gmail.com>
2026-04-19 12:42:10 +0200

    feat: imported services inside the program.cs file

--- backend/CommunityWorkshopOrganizer/Program.cs
+++ backend/CommunityWorkshopOrganizer/Program.cs
@@ -1,5 +1,7 @@
 using CommunityWorkshopOrganizer.Data;
 using Microsoft.EntityFrameworkCore;
+using CommunityWorkshopOrganizer.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
